be4432c10d3551193d8f67bc209eaf9635bd9a7f
Art <arthurluiz36@gmail.com>
2022-02-21 18:13:56 -0300

    Repositorio de relatorios para historico

---
+++ src/main/java/com/Projeto1/SFinanceiro/domain/repository/RelatorioRepository.java
@@ -0,0 +1,9 @@
+package com.Projeto1.SFinanceiro.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Projeto1.SFinanceiro.domain.model.Relatorio;
+
+public interface RelatorioRepository extends JpaRepository<Relatorio, Long>{
+
+}
