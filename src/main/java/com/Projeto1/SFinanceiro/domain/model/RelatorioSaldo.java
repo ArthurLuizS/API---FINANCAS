6a8f6f48fab74ba9f81b363f4dec2bb919edd30b
Art <arthurluiz36@gmail.com>
2022-02-28 19:07:28 -0300

    estrutura do segundo relatorio, de saldo dos clientes

--- src/main/java/com/Projeto1/SFinanceiro/domain/model/RelatorioSaldo.java
+++ src/main/java/com/Projeto1/SFinanceiro/domain/model/RelatorioSaldo.java
@@ -9,8 +9,9 @@ import lombok.Setter;
 @Setter
 public class RelatorioSaldo {
 	private String cliente;
-	private Contas contas;
-	private OffsetDateTime data;
+	private OffsetDateTime dataCliente;
+	private OffsetDateTime dataSaldo;
+	private Float saldo;
 	/*Cliente: X - Cliente desde: DD/MM/YYYY – Saldo em DD/MM/YYYY: 0.000,00
 Cliente: Y - Cliente desde: DD/MM/YYYY - Saldo em DD/MM/YYYY: 000,00
 Cliente: Z - Cliente desde: DD/MM/YYYY - Saldo em DD/MM/YYYY: 00,00*/
