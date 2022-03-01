22ae451e3db79ee1e16655b82941a19709efeda2
Art <arthurluiz36@gmail.com>
2022-03-01 16:34:52 -0300

    alterando apenas a ordem dos atributos

--- src/main/java/com/Projeto1/SFinanceiro/domain/model/RelatorioSaldo.java
+++ src/main/java/com/Projeto1/SFinanceiro/domain/model/RelatorioSaldo.java
@@ -10,8 +10,8 @@ import lombok.Setter;
 public class RelatorioSaldo {
 	private String cliente;
 	private OffsetDateTime dataCliente;
+	private Float saldo = 0F;
 	private OffsetDateTime dataSaldo;
-	private Float saldo;
 	/*Cliente: X - Cliente desde: DD/MM/YYYY – Saldo em DD/MM/YYYY: 0.000,00
 Cliente: Y - Cliente desde: DD/MM/YYYY - Saldo em DD/MM/YYYY: 000,00
 Cliente: Z - Cliente desde: DD/MM/YYYY - Saldo em DD/MM/YYYY: 00,00*/
