25cf006537050070069eb119a0a28d56fc51dfd2
Art <arthurluiz36@gmail.com>
2022-03-05 15:48:36 -0300

    Entidade de input, para receber datas de inicio e fim

---
+++ src/main/java/com/Projeto1/SFinanceiro/api/Model/input/DataInput.java
@@ -0,0 +1,11 @@
+package com.Projeto1.SFinanceiro.api.Model.input;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DataInput {
+private String dataInicio;
+private String dataFim;
+}
