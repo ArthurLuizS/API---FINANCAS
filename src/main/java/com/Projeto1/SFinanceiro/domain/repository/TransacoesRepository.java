5dabb80d4ed4a80bb04340bb6ed968ba8cdfbfec
Art <arthurluiz36@gmail.com>
2022-03-19 16:48:43 -0300

    Repositorio de transacoes, adicionado a busca por data

--- src/main/java/com/Projeto1/SFinanceiro/domain/repository/TransacoesRepository.java
+++ src/main/java/com/Projeto1/SFinanceiro/domain/repository/TransacoesRepository.java
@@ -1,11 +1,17 @@
 package com.Projeto1.SFinanceiro.domain.repository;
 
+import java.time.OffsetDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.Projeto1.SFinanceiro.domain.model.Cliente;
 import com.Projeto1.SFinanceiro.domain.model.Transacoes;
 
 @Repository
 public interface TransacoesRepository extends JpaRepository<Transacoes, Long> {
+
+	List<Transacoes> findByData(OffsetDateTime data);;
 
 }
